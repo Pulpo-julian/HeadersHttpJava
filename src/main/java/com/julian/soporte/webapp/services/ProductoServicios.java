0eaafb60612e89121e7b81e7382bb20badf76160
Pulpo-julian <julian.schrodinger@gmail.com>
2022-08-16 04:43:00 -0500

    añado interface de productos

--- src/main/java/com/julian/soporte/webapp/services/ProductoServicios.java
+++ src/main/java/com/julian/soporte/webapp/services/ProductoServicios.java
@@ -1,5 +1,11 @@
 package com.julian.soporte.webapp.services;
 
+import java.util.List;
+
+import com.julian.soporte.webapp.headers.models.Producto;
+
 public interface ProductoServicios {
+	
+	List<Producto> listar();
 
 }
